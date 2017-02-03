155ef1665ed8fd346bf8ecdc7399920b3872e357
Shuxin Zheng (MSR Student-Person Consulting) <v-shuzhe@microsoft.com>
2017-02-03 15:59:08 +0800

    temp update

--- include/multiverso/updater/dcasgd_updater.h
+++ include/multiverso/updater/dcasgd_updater.h
@@ -1,5 +1,12 @@
-#pragma once
-
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under custom Microsoft Research License Terms for
+// Delayed Compensation Async Stochastic Gradient Descent.
+// See LICENSE.md file in the project root for full license information.
+//
+// See https://arxiv.org/abs/1609.08326 for the details.
+//
 #ifndef MULTIVERSO_UPDATER_DCASGD_UPDATER_H_
 #define MULTIVERSO_UPDATER_DCASGD_UPDATER_H_
 
@@ -17,30 +24,49 @@ namespace multiverso {
 			size_(size) {
 			Log::Debug("[DC-ASGDUpdater] Init. \n");
 			shadow_copies_.resize(isPipeline ? MV_NumWorkers() * 2 : MV_NumWorkers(), std::vector<T>(size_));
-			smooth_gradient_.resize(size_);
+			mean_square_.resize(MV_NumWorkers(), std::vector<T>(size_));
+			for (int i = 0; i < MV_NumWorkers(); ++i)
+			{
+				for (int j = 0; j < size_; ++j)
+				{
+					mean_square_[i][j] = 0.;
+				}
+			}
 		}
 
-		void Update(size_t num_element, T*data, T*delta,
+		void Update(size_t num_element, T* data, T* delta,
 			AddOption* option, size_t offset) override {
+			float e = 1e-10;
+			//fprintf(stderr, "data:%f, lr:%f, delta:%f, lambda:%f, ms:%f, sc:%f, ", data[0], option->learning_rate(), delta[0], option->lambda(), mean_square_[option->worker_id()][0], shadow_copies_[option->worker_id()][0]);
 			for (size_t index = 0; index < num_element; ++index) {
+				T g = delta[index] / option->learning_rate();
+				mean_square_[option->worker_id()][index + offset] *= option->momentum();
+				mean_square_[option->worker_id()][index + offset] += (1 - option->momentum()) * g * g;
+
 				data[index + offset] -= option->learning_rate() *
-					(delta[index] / option->learning_rate() + option->lambda() *
-						std::abs(delta[index] / option->learning_rate()) *
+					(g + option->lambda() / sqrt(mean_square_[option->worker_id()][index + offset] + e)*
+						g * g *
 						(data[index + offset] - shadow_copies_[option->worker_id()][index + offset]));
 
-				// caching each worker's latest version of parameter
+				//caching each worker's latest version of parameter
 				shadow_copies_[option->worker_id()][index + offset] = data[index + offset];
 			}
 		}
 
+		void Access(size_t num_element, T* data, T* blob_data,
+			size_t offset, AddOption*) override {
+			memcpy(blob_data, data + offset, sizeof(T) * num_element);
+		}
+
 		~DCASGDUpdater() {
 			shadow_copies_.clear();
-			smooth_gradient_.clear();
+			mean_square_.clear();
 		}
 
 	protected:
 		std::vector< std::vector<T>> shadow_copies_;
-		std::vector<T> smooth_gradient_;
+		std::vector< std::vector<T>> mean_square_;
+
 		size_t size_;
 	};
 }
